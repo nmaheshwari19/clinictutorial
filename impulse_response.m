d8730a26a6ec79ef6029cb5a1b1ec6b35e6e26b0
Nisha Maheshwari <autholicious@gmail.com>
2017-09-10 21:18:49 -0700

    Changed the impulse response function.

--- impulse_response.m
+++ impulse_response.m
@@ -1,3 +1,6 @@
+% Name: Nisha Maheshwari 
+% Date: 09/10/2017
+
 function [ h,t ] = impulse_response( x, y, fs )
 %impulse_response returns the impulse response of the system given the
 %system input, x, and system output, y.
@@ -8,8 +11,10 @@ function [ h,t ] = impulse_response( x, y, fs )
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
-H = X./Y;   % the calculated frequency response
+H = Y./X;   % the calculated frequency response
 [h,t] = tdomain(H,fs);
+
+plot(t,h);
 
 end
 
